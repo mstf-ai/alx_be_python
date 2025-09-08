13a0ef289008bcf001701970db2eed7bf5f2876a
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-09 01:11:51 +0300

    M

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+# File: rectangle_area.py
+
+# Variables
+length = 10
+width = 5
+
+# Area formula: length × width
+area = length * width
+
+# Output
+print("The area of the rectangle is:", area)
+
