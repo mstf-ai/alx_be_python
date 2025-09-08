86c4f114c67d35040055deeee41c06a8eb4039f2
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-09 01:06:37 +0300

    M

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# File: simple_interest.py
+
+# Variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Simple Interest Formula: I = P * R * T
+interest = principal * rate * time
+
+# Output
+print("The simple interest is:", interest)
