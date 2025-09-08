ab28f0b3c5ef9aed3832dee35fd0bbe3a84ad7e1
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-09 01:28:41 +0300

    M

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+# File: hours_to_seconds.py
+
+# Variable for hours
+hours = 2
+
+# Conversion: 1 hour = 3600 seconds
+seconds = hours * 3600
+
+# Output
+print(hours, "hour(s) is", seconds, "seconds.")
+
