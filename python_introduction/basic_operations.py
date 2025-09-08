800c3345c8daae2e1584d65b5708fbb911264b9c
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-09 01:03:16 +0300

    M

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,16 +1,13 @@
 # File: basic_operations.py
 
-# Define the numbers
 number1 = 10
 number2 = 5
 
-# Perform operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+# Addition
+print("Addition of", number1, "and", number2, "is", number1 + number2)
 
-# Print results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+# Subtraction
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
 
+# Multiplication
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
